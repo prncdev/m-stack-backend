c318508421ccb7ebb35a3fe75d56a51a8b788c08
prncdev <princedavi94@gmail.com>
2024-04-16 09:48:27 +0530

    Cross origin resource sharing, restriction has fix

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express'); 
+const cors = require('cors');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
 
@@ -14,11 +15,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/api/users', require('../routers/GetUsers'));
 
 app.use(errorHandler);
-
 
 app.listen(PORT, function(e) {
   console.log(`http://localhost:${PORT}/api/users`.magenta.underline);
